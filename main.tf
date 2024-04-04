f492c1764c620c270e04bf864f86adc745245397
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-04-04 09:50:09 +0530

    Create main.tf
    commit2

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAX75DMDWK76O3NIMV"
+  secret_key = "jGkEV3nn0PZwbBceFamYMS/cM8K7JlI5pjbl6B1X"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
