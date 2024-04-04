e1e51113edea9a4b2171e6e82eba839bb43df6af
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-04-04 09:54:39 +0530

    Update main.tf1

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-051f8a213df8bc089"
   instance_type = "t2.micro"
 
   tags = {
